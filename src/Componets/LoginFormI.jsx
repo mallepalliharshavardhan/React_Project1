4007fc009ee64e6e870f80960b04e24dcf4976a1
mallepalliharshavardhan <harsha95055@gmail.com>
2026-09-15 23:08:55 +0530

    styling with Tailwind CSS and used UseEffect in LoginformI

--- src/Componets/LoginFormI.jsx
+++ src/Componets/LoginFormI.jsx
@@ -1,8 +1,11 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 let LoginformI=()=>{
     let [email,setEmail]= useState("");
     let [pass,setPass] = useState("");
     let [feedBack,setFeedBack]= useState("")
+
+    useEffect(()=>{console.log(email)},[email]);
+
     let handleSubmit=(event)=>{
        event.preventDefault();
         if(email.trim() === "" || pass === " "){
@@ -19,14 +22,20 @@ let LoginformI=()=>{
         }
     return(
         <>
-        <form onSubmit={handleSubmit}>
+        <div className="bg-green-200 gap-y-3 width-full height-screenjustify-center items-centre rounded-md px-4 py-5 shadow-lg box-border gap-4  ">
+            <form onSubmit={handleSubmit}>
             <h3>LoginForm</h3>
+            <p>Login to continue your practise</p>
             <p>Email:</p>
-            <input onChange={(event)=>setEmail(event.target.value)} type="email" placeholder="Enter email" value={email}/>
-            <input onChange={(event)=>setPass(event.target.value)} type="password" placeholder="Enter password" value={pass}/>
-            <button    type="submit"   >Login</button>
+            <input className="bg-transparent border border-black rounded-sm shadow" onChange={(event)=>setEmail(event.target.value)} type="email" placeholder="Enter email" value={email}/>
+           <p>Password:</p>
+            <input className="bg-transparent border border-black rounded-sm " onChange={(event)=>setPass(event.target.value)} type="password" placeholder="Enter password" value={pass}/>
+            <br/>
+            <button  className="bg-blue-400 rounded-xl hover:bg-blue-600 transition-colors px-3 py-1"  type="submit" >Login</button><br/>
             {feedBack}
         </form>
+        </div>
+        
         </>
     );
 }
